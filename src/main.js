631fb1320ed0159ef9ca1f8e4726042d1ae542be
FuminaMIYAHARA <miyahara.fumina@nttcom.co.jp>
2017-04-25 13:32:18 +0900

    React準備完了

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log("OK");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class LikeButton extends React.Component {
+    render() {
+        return (
+          <span>いいねボタン予定地</span>
+        );
+    }
+}
+
+ReactDOM.render(
+    <LikeButton />,
+    document.getElementById('like-button')
+);
