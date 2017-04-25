a369ad959396d5b130844df5a81653d6d23e059d
FuminaMIYAHARA <miyahara.fumina@nttcom.co.jp>
2017-04-25 14:49:51 +0900

    クリック時のイベントハンドラを追加

--- src/main.js
+++ src/main.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom';
 import styles from './main.css';
 
 class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 999,     // カウント数の状態
+            liked: false    // 押したかどうかの状態
+        }
+    }
+    
+    /* クリックしたときのイベントハンドラ */
+    onClick() {
+        this.setState({
+            count: this.state.count + (this.state.liked ? -1 : 1),
+            liked: !this.state.liked
+        });
+    }
+    
     render() {
         return (
             <span className={styles.container}>
-                <span className={styles.like}>いいね!</span>
+                <span className={styles.like} onClick={(event) => this.onClick()}>
+                    {this.state.liked ? '✔︎' : ''}いいね!
+                </span>
                 <span className={styles.counter}>
-                    <span className={styles.counterBefore}>{" "}</span>999
+                    <span className={styles.counterBefore}>{" "}</span>{this.state.count}
                 </span>
             </span>
         );
