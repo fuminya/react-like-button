d8405674c36b7e1ace9b8cc27327c78463866d8b
FuminaMIYAHARA <miyahara.fumina@nttcom.co.jp>
2017-04-25 14:13:45 +0900

    いいね!ボタンのCSSを適用

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from './main.css';
 
 class LikeButton extends React.Component {
     render() {
         return (
-          <span>いいねボタン予定地</span>
+            <span className={styles.container}>
+                <span className={styles.like}>いいね!</span>
+                <span className={styles.counter}>
+                    <span className={styles.counterBefore}>{" "}</span>999
+                </span>
+            </span>
         );
     }
 }
